35598b32a7aa1004c3ecc146dbd35152ea47ea65
ThiagoFlexa <thiago_flexa@hotmail.com>
2023-01-30 19:59:33 -0300

    atualizando repo

--- exercicios_dia_01/exercicios.py
+++ exercicios_dia_01/exercicios.py
@@ -1,1 +1,22 @@
-test
+print(2 + 2)
+a = 10
+b = 5
+
+#Exercicio1:
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+#Exercicio2:
+hours = 6
+minutes = hours * 60
+print(minutes)
+seconds = minutes * 60
+print(seconds)
+
+#Exercicio4:
+total_valor_pedido = 60 * (24.2 * 0.6 + 0.75) + 3
+print(total_valor_pedido)
